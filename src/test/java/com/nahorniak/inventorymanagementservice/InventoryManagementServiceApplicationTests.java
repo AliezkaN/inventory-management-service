e23d4ab2bf90846c7ff4ef22a97e7302d20a5be9
Oleh Nahorniak <nagornyak68@gmail.com>
2024-04-14 15:00:57 +0300

    project init

---
+++ src/test/java/com/nahorniak/inventorymanagementservice/InventoryManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahorniak.inventorymanagementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagementServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
