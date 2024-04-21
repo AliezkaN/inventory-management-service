ddae82ffd3bbaba6460f2c09ac74808c1445a422
Oleh Nahorniak <nagornyak68@gmail.com>
2024-04-21 22:12:08 +0300

    configure application context path

--- src/main/java/com/nahorniak/inventorymanagementservice/InventoryManagementServiceApplication.java
+++ src/main/java/com/nahorniak/inventorymanagementservice/InventoryManagementServiceApplication.java
@@ -2,12 +2,19 @@ package com.nahorniak.inventorymanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class InventoryManagementServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(InventoryManagementServiceApplication.class, args);
+    }
+
+    @Bean public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+        return factory -> factory.setContextPath("/retail-flow-manager/api");
     }
 
 }
